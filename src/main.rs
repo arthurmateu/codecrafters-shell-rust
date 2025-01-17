e252ab8b05f2a996b1775cf525ad47d8f8777598
admouse <arthurmateu@gmail.com>
2025-01-17 01:35:19 +0100

    repl

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,14 @@ pub fn run_command(command: String) -> String {
 }
 
 fn main() {
-    // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
 
-    println!("{}", run_command(input.trim().to_string()));
+        println!("{}", run_command(input.trim().to_string()));
+    }
 }
