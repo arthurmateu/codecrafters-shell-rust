3944bdf3604bed138cea51c37fb93aaab9c57b79
admouse <arthurmateu@gmail.com>
2025-01-17 01:32:00 +0100

    invalid command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+pub fn run_command(command: String) -> String {
+    format!("{command}: command not found")
+}
+
 fn main() {
     // Uncomment this block to pass the first stage
     print!("$ ");
@@ -10,4 +14,6 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+
+    println!("{}", run_command(input.trim().to_string()));
 }
