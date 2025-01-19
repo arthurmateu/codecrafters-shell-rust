ef2319a793be9cf7e6e21b5bb7c9bb3bfd466137
admouse <arthurmateu@gmail.com>
2025-01-19 22:48:32 +0100

    refactoring + builtins

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,30 @@ use std::{
     process::ExitCode,
 };
 
+fn echo(input: Vec<&str>) -> () {
+    for w in input {
+        print!("{w} ");
+    }
+    println!();
+}
+
+fn command_type(input: Vec<&str>) -> () {
+    let valid_commands = vec!["echo", "type", "exit"];
+
+    for cmd in input {
+        if valid_commands.contains(&cmd) {
+            println!("{} is a shell builtin", cmd);
+        } else {
+            println!("{}: not found", cmd);
+        }
+    }
+}
+
+fn exit_shell(input: &str) -> ExitCode {
+    let status = input.parse::<u8>().unwrap_or(0);
+    ExitCode::from(status)
+}
+
 fn main() -> ExitCode {
     loop {
         print!("$ ");
@@ -14,18 +38,26 @@ fn main() -> ExitCode {
         stdin.read_line(&mut input).unwrap();
         input = input.trim().to_string();
 
-        // A match case below could be better, probably.
-        if let Some(command) = input.split(' ').next() {
-            if command == "echo" {
-                for w in input.split(' ').skip(1) {
-                    print!("{w} ");
-                }
-                println!();
-            } else if command == "exit" {
-                return ExitCode::from(0);
-            } else {
-                println!("{}: command not found", input);
-            }
+        let mut command_args: Vec<&str> = input.split_whitespace().collect();
+        let mut command: &str = " ";
+
+        if command_args.len() > 0 {
+            command = command_args[0];
+        }
+        if command_args.len() > 1 {
+            command_args = command_args[1..].to_vec();
+        }
+
+        if command == "echo" {
+            echo(command_args);
+        } else if command == "type" {
+            command_type(command_args);
+        } else if command == "exit" {
+            return exit_shell(command_args[0]);
+        } else if command == " " {
+            // do nothing
+        } else {
+            println!("{}: command not found", command);
         }
     }
 }
