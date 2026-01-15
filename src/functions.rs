48e36397fff7ebf68e39dace0cda9cb8013b2e7c
arthurmateu <arthurmateu@gmail.com>
2026-01-15 21:00:16 +0100

    codecrafters submit [skip ci]

--- src/functions.rs
+++ src/functions.rs
@@ -1,6 +1,5 @@
 use std::{
-    collections::HashSet, 
-    process::ExitCode,
+    collections::HashSet, env, os::unix::fs::PermissionsExt, path::{Path, PathBuf}, process::ExitCode
 };
 
 pub fn echo(input: Vec<&str>) -> () {
@@ -8,14 +7,36 @@ pub fn echo(input: Vec<&str>) -> () {
 }
 
 pub fn command_type(input: Vec<&str>) -> () {
-    let hs = HashSet::from(["echo", "type", "exit"]);
+    let builtins = HashSet::from(["echo", "type", "exit"]);
     let cmd = input.first().unwrap();
+    let full_path = which(cmd);
 
-    if hs.contains(cmd) {
+    if builtins.contains(cmd) {
         println!("{} is a shell builtin", cmd);
+    } else if full_path.is_some() {
+        println!("{} is {}", cmd, full_path.unwrap());
     } else {
         println!("{}: not found", cmd);
     }
+}
+
+fn which(cmd: &str) -> Option<String> {
+    env::var("PATH")
+        .unwrap()
+        .split(":")
+        .map(Path::new)
+        .find_map(|dir| {
+            let full = dir.join(cmd);
+            if full.is_file() && is_executable(&full) {
+                full.into_os_string().into_string().ok()
+            } else {
+                None
+            }
+        })
+}
+
+fn is_executable(cmd: &PathBuf) -> bool {
+    cmd.metadata().unwrap().permissions().mode() & 0o111 != 0
 }
 
 pub fn exit_shell(input: &str) -> ExitCode {
